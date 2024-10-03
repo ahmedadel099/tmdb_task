8aed6c8296a312620251d7846b4c2ff840f275cb
ahmedadel099 <am4118327@gmail.com>
2024-10-03 15:33:12 +0300

    updated  the dependency injection to include Hive and NetworkInfo

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,26 +1,33 @@
+// lib/core/di/injection_container.dart
+
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import '../../data/models/person.dart';
 import '../../data/repositories/person_repository.dart';
-import '../../presentation/bloc/person_image_bloc.dart';
 import '../api/api_client.dart';
-import '../../presentation/bloc/person_bloc.dart';
+import '../network/network_info.dart';
 
 final getIt = GetIt.instance;
 
-void setupDependencies() {
+Future<void> setupDependencies() async {
+  // Hive
+  await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
+  final personBox = await Hive.openBox<Person>('persons');
+
   // Register Dio
   getIt.registerLazySingleton(() => Dio());
 
   // Register ApiClient
   getIt.registerLazySingleton(() => ApiClient(getIt<Dio>()));
 
+  // Register NetworkInfo
+  getIt.registerLazySingleton(() => NetworkInfo(Connectivity()));
+
   // Register PersonRepository
-  getIt.registerLazySingleton(() => PersonRepository(getIt<ApiClient>()));
-
-  // Register BLoCs
-  getIt
-      .registerFactory(() => PersonBloc(repository: getIt<PersonRepository>()));
-
-  getIt.registerFactory(
-      () => PersonImagesBloc(repository: getIt<PersonRepository>()));
+  getIt.registerLazySingleton(
+      () => PersonRepository(getIt<ApiClient>(), personBox));
 }
